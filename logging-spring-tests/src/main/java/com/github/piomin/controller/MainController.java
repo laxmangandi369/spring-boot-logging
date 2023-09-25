4fe0f91680282ef3130404acf3a85a65ccf9006f
laxmangandi369 <119059799+laxmangandi369@users.noreply.github.com>
2023-09-25 17:40:23 +0530

    Update MainController.java

--- logging-spring-tests/src/main/java/com/github/piomin/controller/MainController.java
+++ logging-spring-tests/src/main/java/com/github/piomin/controller/MainController.java
@@ -19,7 +19,7 @@ public class MainController {
     public String postWithId(@RequestBody Integer id) {
         return "Hello-" + id;
     }
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;}}}}}}}}}}}}}}
     @GetMapping("/req-param")
     public String findByIdRequest(@RequestParam Integer id) {
         return "Hello-" + id;
